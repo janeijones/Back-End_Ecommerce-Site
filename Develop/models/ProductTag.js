a6b95692f871768e1ce2311802ebc4d74b293c85
Janei <18485105+janeijones@users.noreply.github.com>
2021-05-09 21:56:51 -0500

    Added id column for ProductTag table

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,14 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    
   },
   {
     sequelize,
