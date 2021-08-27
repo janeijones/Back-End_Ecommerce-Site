242e666a8337f4d687c5dbf385da1c8116b02788
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-27 03:19:49 -0500

    Update indiviidual category by ID, with errror handling

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -28,11 +28,9 @@ try {
       res.status(200).json(findAllCategories)
     }
   } catch (err) {
-
     //error handling
     res.status(500).json(err)
   }
-
 });
 
 router.get('/:id', (req, res) => {
@@ -42,10 +40,41 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const createCategory = await Category.create({
+      category_name: req.body.category_name
+    })
+
+    //if successful, create category
+    res.status(200).json(createCategory)
+  } catch (err) {
+    //error handling
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  try {
+    // update a category by its `id` value
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    //if category cannot be updated
+    if (!updateCategory){
+      res.status(404).json({
+        message: 'Category cannot be updated!'
+      })
+      return
+    } else {
+      //else, update category
+      res.status(200).json(updateCategory)
+    }
+  } catch (err) {
+    // Error handling
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
